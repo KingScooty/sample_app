c3396f85067b7b7266194ba19a3e3817f9797022
Scotty Vernon <scott.vernon@gmail.com>
2014-07-19 22:08:33 +0100

    Micropost shizzle.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -53,15 +54,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-
-    def signed_in_user
-      # redirect_to signin_url, notice: "Please sign in." unless signed_in?
-      unless signed_in?
-        store_location
-        flash[:warning] = "Please sign in."
-        redirect_to signin_url
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
